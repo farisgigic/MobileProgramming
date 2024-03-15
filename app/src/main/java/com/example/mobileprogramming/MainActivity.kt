4c53bf77684b07f52e356d94264cbb9e1a3e3cbf
Faris <faris.gigic@stu.ibu.edu.ba>
2024-03-15 10:39:38 +0100

    Lecture

--- app/src/main/java/com/example/mobileprogramming/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.mobileprogramming
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mobileprogramming.ui.theme.MobileProgrammingTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +25,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MobileProgrammingTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.primary
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    println()
+                    MessageCard(name = com.example.mobileprogramming.Message("Faris","Hello there"))
                 }
             }
         }
@@ -30,10 +40,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        //modifier = modifier
     )
 }
 
@@ -44,9 +54,17 @@ fun GreetingPreview() {
         Greeting("FARIS")
     }
 }
-
+data class Message(val author: String, val body : String)
 @Composable
-fun Greeting1(name : String)
-{
-    Text(text = "My name is $name")
+fun MessageCard(name : com.example.mobileprogramming.Message){
+
+    Column {
+        Text(text = name.author)
+        Text(text = name.body)
+    }
+}
+@Preview
+@Composable
+fun MessageCardPreview(){
+    MessageCard(name = com.example.mobileprogramming.Message("Faris","Hello there"))
 }
