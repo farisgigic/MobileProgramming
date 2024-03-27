af1dc678dab9380f890bf7c0449ec41dac92b4a1
Faris <faris.gigic@stu.ibu.edu.ba>
2024-03-27 12:09:13 +0100

    Dice Roller

--- app/src/main/java/com/example/mobileprogramming/04_DiceRoller.kt
+++ app/src/main/java/com/example/mobileprogramming/04_DiceRoller.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
